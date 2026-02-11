9930cd376e99b9750cc084f35f0f5fe49da768a5
Varun-9997 <dangrevarun@gmail.com>
2026-02-11 16:17:37 +0530

    website folder added

---
+++ website/conn.php
@@ -0,0 +1,18 @@
+<?php
+$server = "localhost";
+$username = "root";
+// $username = "anushkaf_granthaa";
+$password = "";
+// $password = "oys1e)8uz3@,jE-1";
+$dbname = "anushkaf_granthaa";
+
+	  try {
+         $conn = new PDO("mysql:host=$server;dbname=$dbname", $username, $password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+		   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		   date_default_timezone_set("Asia/Kolkata");
+        //    echo "database connected !!";
+      } catch (\Throwable $th) {
+           echo $th;
+      }
+
+?> 
---
+++ website/footer.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container-fluid d-flex justify-content-center">
+      <div class="copyright">
+        2026, made with <i class="fa fa-heart heart text-danger"></i> by
+        <a target="_blank" href="https://softgrowthinfotech.com/">Softgrowth Infotech</a>
+      </div>
+    </div>
+  </footer>
