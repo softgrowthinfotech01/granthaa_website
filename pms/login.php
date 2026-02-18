7fc7c48beb455cbf9f879c2437367ba8c68604c8
Kaustubh Lande <kaustubhlande52@gmail.com>
2026-02-18 14:44:29 +0530

    login

--- pms/login.php
+++ pms/login.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Leader Login | Granthaa Land Developer Pvt Ltd</title>
+<title>User Login | Granthaa Land Developer Pvt Ltd</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <link rel="stylesheet" href="style.css">
