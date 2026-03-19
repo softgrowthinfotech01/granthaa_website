33ec8ed0f1985b8f0690ac237a9620aa948f7a29
Kaustubh Lande <kaustubhlande52@gmail.com>
2026-03-19 14:57:45 +0530

    ss

--- pms/tailwind.config.js
+++ pms/tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./**/*.html",
     "./**/*.js"
   ],
+  safelist: [
+  {
+    pattern: /(from|to)-(orange|green|blue|yellow|indigo|purple)-(100|200|300|400|500|600|700|800|900)/
+  },
+  'bg-gradient-to-br'
+],
   theme: {
     extend: {},
   },
