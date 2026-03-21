f524899ad2c818f94a4bd368de20baf78650266b
aniketxx <aniketparkhi2004@gmail.com>
2026-03-21 15:14:22 +0530

    team

--- pms/leader/dashboard.php
+++ pms/leader/dashboard.php
@@ -29,7 +29,7 @@
 
     <div class="absolute top-5 right-5 opacity-50 text-6xl font-bold">₹</div>
 
-    <h3 class="text-sm tracking-widest font-semibold">TOTAL MY BOOKING AMOUNT</h3>
+    <h3 class="text-sm tracking-widest font-semibold">TOTAL TEAM BOOKING AMOUNT</h3>
     <p class="text-3xl font-bold mt-2" id="teamtotalBooking"></p>
   </div>
 
