5872d7e01636b9a3045995c351986b8ca42f8559
aniketxx <aniketparkhi2004@gmail.com>
2026-03-24 14:56:44 +0530

    url

--- pms/url.js
+++ pms/url.js
@@ -1,2 +1,2 @@
-// const url = "https://granthaadeveloperpvtltd.com/pms/apis/api/";
-const url = "http://127.0.0.1:8000/api/";
+const url = "https://granthaadeveloperpvtltd.com/pms/apis/api/";
+// const url = "http://127.0.0.1:8000/api/";
