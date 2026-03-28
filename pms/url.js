5133d87d220087a1fe5e3bbe9d9a23bf2444d815
aniketxx <aniketparkhi2004@gmail.com>
2026-03-28 12:16:51 +0530

    scsodc dicndnd

--- pms/url.js
+++ pms/url.js
@@ -1,3 +1,4 @@
 const url = "https://granthaadeveloperpvtltd.com/pms/apis/api/";
 // const url = "http://127.0.0.1:8000/api/";
-const base_url = "http://127.0.0.1:8000/";
+const base_url = "https://granthaadeveloperpvtltd.com/pms/apis/";
+// const base_url = "http://127.0.0.1:8000/";
