2c28270a117013d340d54130aedccaa0ef1215aa
aniketxx <aniketparkhi2004@gmail.com>
2026-04-03 04:59:53 +0530

    Add PAN Card input field to Add Leader form

--- pms/admin/add_leader.php
+++ pms/admin/add_leader.php
@@ -83,6 +83,13 @@
                                             class="w-full border border-gray-400 rounded-lg bg-white text-sm focus:ring-2 focus:ring-blue-500 outline-none p-2 bg-white">
                                     </div>
 
+                                    <div>
+                                        <label class="block mb-1 text-sm font-medium text-gray-700">Pan Card</label>
+                                        <input name="age" type="text" id="pancard_number"
+                                            class="w-full px-3 py-2.5 border border-gray-400 rounded-lg bg-white text-sm focus:ring-2 focus:ring-blue-500 outline-none"
+                                            placeholder="Enter pancard_number number" required>
+                                    </div>
+
                                 </div>
                             </div>
 
