2888626762db49f43740d72a118bd585ec8babe0
aniketxx <aniketparkhi2004@gmail.com>
2026-04-03 05:06:07 +0530

    Fix input name for PAN Card field in Add Leader form and update API URLs for local development

--- pms/admin/add_leader.php
+++ pms/admin/add_leader.php
@@ -85,7 +85,7 @@
 
                                     <div>
                                         <label class="block mb-1 text-sm font-medium text-gray-700">Pan Card</label>
-                                        <input name="age" type="text" id="pancard_number"
+                                        <input name="pancard_number" type="text" id="pancard_number"
                                             class="w-full px-3 py-2.5 border border-gray-400 rounded-lg bg-white text-sm focus:ring-2 focus:ring-blue-500 outline-none"
                                             placeholder="Enter pancard_number number" required>
                                     </div>
