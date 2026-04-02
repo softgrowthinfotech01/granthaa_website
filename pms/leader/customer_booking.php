78f2bf20b856159559294282dadaf3c319bc6d7d
Varun-9997 <dangrevarun@gmail.com>
2026-04-02 16:09:30 +0530

    cd

--- pms/leader/customer_booking.php
+++ pms/leader/customer_booking.php
@@ -192,7 +192,7 @@
                 </div>
 
                 <div class="space-y-2">
-                    <label class="text-sm font-semibold text-gray-700">Total Booking Amount</label>
+                    <label class="text-sm font-semibold text-gray-700">Total Amount</label>
                     <input name="total_booking_amount" id="total_booking_amount" type="number"
                         class="w-full border border-gray-300 px-5 py-3 rounded-xl focus:ring-2 focus:ring-yellow-400 outline-none">
                 </div>
