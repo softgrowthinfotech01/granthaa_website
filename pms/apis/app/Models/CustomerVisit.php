618d32a99e17e69e54a61fbe07e3a3b7b051f8cd
aniketxx <aniketparkhi2004@gmail.com>
2026-04-05 05:33:24 +0530

    new : created customer visit APIs and added role validation for the bookings create

---
+++ pms/apis/app/Models/CustomerVisit.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CustomerVisit extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'contact_no',
+        'aadhaar_number',
+        'gender',
+        'address',
+        'site_location_id',
+        'remark',
+        'created_by',
+        'released',
+        'visited_at'
+    ];
+
+     // Relations
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo(LocationMaster::class, 'site_location');
+    }
+}
