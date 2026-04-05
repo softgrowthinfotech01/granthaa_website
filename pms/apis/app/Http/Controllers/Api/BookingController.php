c257d57056bc788799318e51bcebfbdbf7e12f13
aniketxx <aniketparkhi2004@gmail.com>
2026-04-05 15:00:27 +0530

    fghng

--- pms/apis/app/Http/Controllers/Api/BookingController.php
+++ pms/apis/app/Http/Controllers/Api/BookingController.php
@@ -482,7 +482,7 @@ class BookingController extends Controller
     {
         $user = auth()->user();
         //  print_r($user);exit;
-        $booking = Booking::whereNull('deleted_at')->where('created_by', $user->id)->get();
+        $booking = Booking::whereNull('deleted_at')->where('user_id', $user->id)->get();
         // print_r($booking);exit;
         return response()->json($booking);
     }
