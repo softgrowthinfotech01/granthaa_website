60ad0f4f4523db116247ca8f979caed057316650
Kaustubh Lande <kaustubhlande52@gmail.com>
2026-04-07 01:14:13 +0530

    sss

--- pms/customer/sidebar.php
+++ pms/customer/sidebar.php
@@ -1,9 +1,11 @@
-<?php $current = basename($_SERVER['PHP_SELF']); ?>
-
+<?php 
+$current = basename($_SERVER['PHP_SELF'], ".php");
+?>
 <aside id="sidebar"
   class="fixed md:static z-30 top-0 left-0 h-full w-55 md:w-64 bg-gray-800 text-white
 transition-all duration-300 ease-in-out -translate-x-full md:translate-x-0
-flex flex-col justify-between shrink-0">
+  flex flex-col justify-between shrink-0 overflow-y-auto scroll-smooth">
+
 
   <!-- Top -->
   <div>
@@ -23,7 +25,7 @@ flex flex-col justify-between shrink-0">
     <nav class="mt-6 space-y-2 px-2">
 
       <a href="dashboard"
-        class="menu-item <?php if ($current == 'dashboard') echo 'active'; ?>">
+        class="menu-item <?php if ($current == 'dashboard') echo 'parent-active'; ?>">
         <!-- home icon -->
         <svg class="w-6 h-6 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path d="M3 12l9-9 9 9M4 10v10h16V10" />
@@ -32,7 +34,7 @@ flex flex-col justify-between shrink-0">
       </a>
 
       <a href="refer_customer"
-        class="menu-item <?php if ($current == 'refer_customer') echo 'active'; ?>">
+        class="menu-item <?php if ($current == 'refer_customer') echo 'parent-active'; ?>">
         <svg class="w-6 h-6 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path d="M12 4v16m8-8H4" />
         </svg>
@@ -40,7 +42,7 @@ flex flex-col justify-between shrink-0">
       </a>
 
       <a href="list_refer_customer"
-        class="menu-item <?php if ($current == 'list_refer_customer') echo 'active'; ?>">
+        class="menu-item <?php if ($current == 'list_refer_customer') echo 'parent-active'; ?>">
         <svg class="w-6 h-6 text-yellow-500" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path d="M3 7h18M3 12h18M3 17h18" />
         </svg>
@@ -48,7 +50,7 @@ flex flex-col justify-between shrink-0">
       </a>
 
       <a href="my_bookings"
-        class="menu-item <?php if ($current == 'my_bookings') echo 'active'; ?>">
+        class="menu-item <?php if ($current == 'my_bookings') echo 'parent-active'; ?>">
         <svg class="w-6 h-6 text-yellow-500" fill="currentColor" viewBox="0 0 24 24">
           <rect x="3" y="4" width="18" height="16" rx="2" />
           <rect x="7" y="8" width="10" height="2" />
@@ -59,7 +61,7 @@ flex flex-col justify-between shrink-0">
       </a>
 
       <a href="my_booking_payment"
-        class="menu-item <?php if ($current == 'my_booking_payment') echo 'active'; ?>">
+        class="menu-item <?php if ($current == 'my_booking_payment') echo 'parent-active'; ?>">
         <svg class="w-6 h-6 text-yellow-500" fill="none" stroke="currentColor" stroke-width="1.8" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" d="M7 3h8l4 4v14l-2-1-2 1-2-1-2 1-2-1-2 1V5a2 2 0 0 1 2-2z" />
           <path stroke-linecap="round" stroke-linejoin="round" d="M15 3v4h4" />
@@ -94,8 +96,8 @@ flex flex-col justify-between shrink-0">
   </div>
 
   <!-- Bottom Logout -->
-  <div class="p-3 border-t border-gray-700">
-    <a href=""
+    <div class="p-3 border-t border-gray-700">
+    <a onclick="logout()" style="cursor: pointer;"
       class="menu-item text-red-400 hover:bg-red-500 hover:text-white transition">
       <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
         <path d="M17 16l4-4m0 0l-4-4m4 4H7" />
@@ -105,3 +107,29 @@ flex flex-col justify-between shrink-0">
   </div>
 
 </aside>
+
+<script>
+
+
+  function logout() {
+    const token = localStorage.getItem("auth_token");
+
+    fetch(url + "logout", {
+      method: "POST",
+      headers: {
+        "Authorization": "Bearer " + token,
+        "Accept": "application/json"
+      }
+    }).finally(() => {
+      localStorage.clear();
+      window.location.href = "../login.php";
+    });
+  }
+
+  const user = JSON.parse(localStorage.getItem("auth_user"));
+
+  if (!user || user.role !== "customer") {
+    alert("Unauthorized access");
+    window.location.href = "../login.php";
+  }
+</script>
