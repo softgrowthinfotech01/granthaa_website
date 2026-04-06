b2deb4d602840cdb9ffa1bf4b1c695f03feb062e
aniketxx <aniketparkhi2004@gmail.com>
2026-04-06 07:37:41 +0530

    scsk

--- pms/url.js
+++ pms/url.js
@@ -1,4 +1,4 @@
-// const url = "https://granthaadeveloperpvtltd.com/pms/apis/api/";
-const url = "http://127.0.0.1:8000/api/";
-// const base_url = "https://granthaadeveloperpvtltd.com/pms/apis/";
-const base_url = "http://127.0.0.1:8000/";
+const url = "https://granthaadeveloperpvtltd.com/pms/apis/api/";
+// const url = "http://127.0.0.1:8000/api/";
+const base_url = "https://granthaadeveloperpvtltd.com/pms/apis/";
+// const base_url = "http://127.0.0.1:8000/";
